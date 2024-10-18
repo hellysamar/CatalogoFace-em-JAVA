b506f9571d231eafb36d3092e0d6a533bb3ab767
Hellysamar Araujo <hellysamar@hotmail.com>
2024-10-18 19:19:38 -0300

    Criado o método de busca por preenchimento do campo Nome, e ao selecionar por index preenche os campos e foto

--- CatalogoFace/src/view/Catalogo.java
+++ CatalogoFace/src/view/Catalogo.java
@@ -24,14 +24,17 @@ import java.text.DateFormat;
 import java.util.Date;
 
 import javax.imageio.ImageIO;
+import javax.swing.DefaultListModel;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
@@ -39,6 +42,8 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 import model.DAO;
 import utils.Validador;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Catalogo extends JFrame {
 
@@ -56,7 +61,7 @@ public class Catalogo extends JFrame {
 	
 
 	private static final long serialVersionUID = 1L;
-	private JPanel btnPesquisar;
+	private JPanel pnl;
 	private JLabel lblStatus;
 	private JLabel lblData;
 	private JLabel lblRegistro;
@@ -64,6 +69,8 @@ public class Catalogo extends JFrame {
 	private JTextField txtNome;
 	private JTextField txtEndereco;
 	private JLabel lblFoto;
+	private JList listaNomes;
+	private JScrollPane scrollPaneNomes;
 
 	/**
 	 * Launch the application.
@@ -96,17 +103,17 @@ public class Catalogo extends JFrame {
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Catalogo.class.getResource("/icons/CF.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 681, 392);
-		btnPesquisar = new JPanel();
-		btnPesquisar.setBackground(new Color(240, 240, 240));
-		btnPesquisar.setBorder(new EmptyBorder(5, 5, 5, 5));
+		pnl = new JPanel();
+		pnl.setBackground(new Color(240, 240, 240));
+		pnl.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-		setContentPane(btnPesquisar);
-		btnPesquisar.setLayout(null);
+		setContentPane(pnl);
+		pnl.setLayout(null);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(128, 128, 255));
 		panel.setBounds(0, 293, 742, 60);
-		btnPesquisar.add(panel);
+		pnl.add(panel);
 		panel.setLayout(null);
 		
 		lblStatus = new JLabel("");
@@ -125,7 +132,7 @@ public class Catalogo extends JFrame {
 		lblRegistro.setForeground(new Color(128, 128, 255));
 		lblRegistro.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblRegistro.setBounds(10, 26, 144, 17);
-		btnPesquisar.add(lblRegistro);
+		pnl.add(lblRegistro);
 		
 		txtRegistro = new JTextField();
 		txtRegistro.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
@@ -140,7 +147,7 @@ public class Catalogo extends JFrame {
 			}
 		});
 		txtRegistro.setBounds(165, 26, 134, 20);
-		btnPesquisar.add(txtRegistro);
+		pnl.add(txtRegistro);
 		txtRegistro.setColumns(10);
 		txtRegistro.setDocument(new Validador(6));
 		
@@ -149,12 +156,18 @@ public class Catalogo extends JFrame {
 		lblNome.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblNome.setForeground(new Color(128, 128, 255));
 		lblNome.setBounds(10, 68, 46, 14);
-		btnPesquisar.add(lblNome);
+		pnl.add(lblNome);
 		
 		txtNome = new JTextField();
+		txtNome.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyReleased(KeyEvent e) {
+				listarNomesBanco();
+			}
+		});
 		txtNome.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		txtNome.setBounds(10, 93, 289, 20);
-		btnPesquisar.add(txtNome);
+		pnl.add(txtNome);
 		txtNome.setColumns(10);
 		txtNome.setDocument(new Validador(30));
 		
@@ -168,7 +181,7 @@ public class Catalogo extends JFrame {
 		btnCarregarFoto.setForeground(new Color(128, 128, 255));
 		btnCarregarFoto.setFont(new Font("Tahoma", Font.BOLD, 11));
 		btnCarregarFoto.setBounds(479, 259, 115, 23);
-		btnPesquisar.add(btnCarregarFoto);
+		pnl.add(btnCarregarFoto);
 		
 		JButton btnAdicionar = new JButton("");
 		btnAdicionar.addActionListener(new ActionListener() {
@@ -180,21 +193,21 @@ public class Catalogo extends JFrame {
 		btnAdicionar.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		btnAdicionar.setIcon(new ImageIcon(Catalogo.class.getResource("/icons/CF check.png")));
 		btnAdicionar.setBounds(10, 192, 90, 90);
-		btnPesquisar.add(btnAdicionar);
+		pnl.add(btnAdicionar);
 		
 		JButton btnEditar = new JButton("");
 		btnEditar.setToolTipText("Editar");
 		btnEditar.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		btnEditar.setIcon(new ImageIcon(Catalogo.class.getResource("/icons/CF edit.png")));
 		btnEditar.setBounds(110, 192, 90, 90);
-		btnPesquisar.add(btnEditar);
+		pnl.add(btnEditar);
 		
 		JButton btnDeletar = new JButton("");
 		btnDeletar.setToolTipText("Deletar");
 		btnDeletar.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		btnDeletar.setIcon(new ImageIcon(Catalogo.class.getResource("/icons/CF delete.png")));
 		btnDeletar.setBounds(210, 192, 90, 90);
-		btnPesquisar.add(btnDeletar);
+		pnl.add(btnDeletar);
 		
 		JButton btnLimpar = new JButton("");
 		btnLimpar.addActionListener(new ActionListener() {
@@ -206,38 +219,55 @@ public class Catalogo extends JFrame {
 		btnLimpar.setIcon(new ImageIcon(Catalogo.class.getResource("/icons/CF change.png")));
 		btnLimpar.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		btnLimpar.setBounds(310, 192, 90, 90);
-		btnPesquisar.add(btnLimpar);
+		pnl.add(btnLimpar);
 		
-		txtEndereco = new JTextField();
-		txtEndereco.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		txtEndereco.setBounds(10, 149, 390, 20);
-		btnPesquisar.add(txtEndereco);
-		txtEndereco.setColumns(10);
-		txtEndereco.setDocument(new Validador(30));
+		scrollPaneNomes = new JScrollPane();
+		scrollPaneNomes.setVisible(false);
+		scrollPaneNomes.setBorder(null);
+		scrollPaneNomes.setBounds(10, 111, 289, 82);
+		pnl.add(scrollPaneNomes);
+		
+		listaNomes = new JList();
+		listaNomes.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				listarNomeBanco();
+			}
+		});
+		listaNomes.setBorder(null);
+		scrollPaneNomes.setViewportView(listaNomes);
 		
 		JLabel lblEndereco = new JLabel("Endereço:");
 		lblEndereco.setBackground(new Color(240, 240, 240));
 		lblEndereco.setForeground(new Color(128, 128, 255));
 		lblEndereco.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblEndereco.setBounds(10, 124, 78, 14);
-		btnPesquisar.add(lblEndereco);
+		pnl.add(lblEndereco);
+		
+		txtEndereco = new JTextField();
+		txtEndereco.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		txtEndereco.setBounds(10, 149, 390, 20);
+		pnl.add(txtEndereco);
+		txtEndereco.setColumns(10);
+		txtEndereco.setDocument(new Validador(30));
 		
 		lblFoto = new JLabel("New label");
 		lblFoto.setAlignmentX(Component.CENTER_ALIGNMENT);
 		lblFoto.setIcon(new ImageIcon(Catalogo.class.getResource("/icons/CF photo.png")));
 		lblFoto.setBounds(410, 11, 229, 242);
-		btnPesquisar.add(lblFoto);
+		pnl.add(lblFoto);
 		
-		JButton btnNewButton_1 = new JButton("");
-		btnNewButton_1.addActionListener(new ActionListener() {
+		JButton btnPesquisar = new JButton("");
+		btnPesquisar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				buscarRegistro();
 			}
 		});
-		btnNewButton_1.setToolTipText("Pesquisar");
-		btnNewButton_1.setIcon(new ImageIcon(Catalogo.class.getResource("/icons/CF search.png")));
-		btnNewButton_1.setBounds(310, 23, 90, 90);
-		btnPesquisar.add(btnNewButton_1);
+		
+		btnPesquisar.setToolTipText("Pesquisar");
+		btnPesquisar.setIcon(new ImageIcon(Catalogo.class.getResource("/icons/CF search.png")));
+		btnPesquisar.setBounds(310, 23, 90, 90);
+		pnl.add(btnPesquisar);
 	}
 	
 	// Método para verificar o Status de conexão
@@ -255,7 +285,6 @@ public class Catalogo extends JFrame {
 			
 			conn.close();
 		} catch (Exception e) {
-			// TODO: handle exception
 			System.out.println(e);
 		}
 	}
@@ -326,6 +355,72 @@ public class Catalogo extends JFrame {
 		}
 	}
 	
+	private void listarNomesBanco() {
+		// criando um vetor dinamico
+		DefaultListModel<String> modelo = new DefaultListModel<>();
+		listaNomes.setModel(modelo);
+		String LerLista = "SELECT * FROM tblUsuarios WHERE nome LIKE '" + txtNome.getText() + "%' ORDER BY nome;";
+		
+		try {
+			conn = dao.conectar();
+			pst = conn.prepareStatement(LerLista);
+			rs = pst.executeQuery();
+			
+			while(rs.next()) {
+				scrollPaneNomes.setVisible(true);
+				modelo.addElement(rs.getString(2));
+				
+				if(txtNome.getText().isEmpty()) {
+					scrollPaneNomes.setVisible(false);
+				}
+			}
+			conn.close();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+//		scrollPaneNomes.setVisible(true);
+	}
+	
+	private void listarNomeBanco() {
+		int linha = listaNomes.getSelectedIndex();
+		if (linha >= 0) {
+			String sql = "SELECT * FROM tblUsuarios WHERE nome LIKE '" + txtNome.getText() + "%' ORDER BY nome LIMIT " + (linha) + ", 1";
+			
+			try {
+				conn = dao.conectar();
+				pst = conn.prepareStatement(sql);
+				rs = pst.executeQuery();
+				
+				while (rs.next()) {
+					scrollPaneNomes.setVisible(false);
+					txtRegistro.setText(rs.getString(1));
+					txtNome.setText(rs.getString(2));
+					txtEndereco.setText(rs.getString(4));
+					
+					Blob blob = (Blob) rs.getBlob(3);
+					byte[] img = blob.getBytes(1, (int) blob.length());
+					
+					BufferedImage imagem = null;
+					try {
+						imagem = ImageIO.read(new ByteArrayInputStream(img));
+					} catch (Exception e) {
+						System.out.println(e);
+					}
+					
+					ImageIcon icone = new ImageIcon(imagem);
+					Icon foto = new ImageIcon(icone.getImage().getScaledInstance(lblFoto.getWidth(), lblFoto.getHeight(), Image.SCALE_SMOOTH));
+					
+					lblFoto.setIcon(foto);
+				}
+			} catch (Exception e) {
+				 System.out.println(e);
+			}
+		} else {
+			scrollPaneNomes.setVisible(false);
+		}
+	}
+	
 	private void buscarRegistro() {		
 		if (txtRegistro.getText().isEmpty()) {
 			JOptionPane.showMessageDialog(null, "Preencha o campo Registro para buscar");
@@ -373,5 +468,6 @@ public class Catalogo extends JFrame {
 		txtEndereco.setText(null);
 		lblFoto.setIcon(new ImageIcon(Catalogo.class.getResource("/icons/CF photo.png")));
 		txtNome.requestFocus();
+		scrollPaneNomes.setVisible(false);
 	}
 }
